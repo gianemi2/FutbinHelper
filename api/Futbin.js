d1d5d0008b699d95b6d74a5c28d9d53764d3d492
Marco Giannini <marco.giannini@6emme.it>
2020-09-01 09:31:14 +0200

    Changed from post to get and insert name as query parameters

--- api/Futbin.js
+++ api/Futbin.js
@@ -2,18 +2,21 @@ import axios from 'axios'
 
 const Futbin = {
     fetchPlayers: async (name) => {
-        var urlencoded = new URLSearchParams();
-        urlencoded.append("playername", name);
 
         // If name length is longer than 3 fetch futbinAPI and return response.
         if (name.length > 3) {
-            const response = await axios.post(`https://www.futbin.org/futbin/api/searchPlayersByName`, urlencoded, {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
+            try {
+                const response = await axios.get(`https://www.futbin.org/futbin/api/searchPlayersByName?playername=${name}`, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                })
+                if (response.data.data) {
+                    return response.data.data;
                 }
-            })
-            if (response.data.data) {
-                return response.data.data;
+            } catch (error) {
+                console.log(error);
+                return { success: false, message: error }
             }
         }
     }
